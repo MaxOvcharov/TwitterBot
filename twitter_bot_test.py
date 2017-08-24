66baaf9cd06157bd8d02c7aeeea15e122d90f465
MaxOvcharov <ovcharovmax@yandex.ru>
2017-08-24 23:03:30 +0300

    Update simple Twitter Bot example - added update_status method

--- twitter_bot_test.py
+++ twitter_bot_test.py
@@ -8,5 +8,6 @@ auth.set_access_token(ACCESS_TOKEN, ACCESS_TOKEN_SECRET)
 api = tweepy.API(auth)
 
 user = api.get_user('twitter')
+api.update_status('Просто так твит ни о чём!')
 
 print(user.screen_name, user.followers_count)
