f952691e5770067d24b964dc1554811989c56d48
MaxOvcharov <ovcharovmax@yandex.ru>
2017-08-27 23:00:14 +0300

    Added simple BOT on Twitter Streaming API

---
+++ twitter_streaming_bot.py
@@ -0,0 +1,35 @@
+import json
+import pprint
+
+from local_settings import ACCESS_TOKEN, ACCESS_TOKEN_SECRET, CONSUMER_KEY, CONSUMER_SECRET
+from tweepy.streaming import StreamListener
+from tweepy import OAuthHandler
+from tweepy import Stream
+
+
+class StdOutListener(StreamListener):
+    """ A listener handles tweets that are received from the stream.
+    This is a basic listener that just prints received tweets to stdout.
+
+    """
+    def on_data(self, data):
+        tweet = json.loads(data)
+        pprint.pprint(tweet)
+        print('\n')
+        # print(tweet.get('text'))
+        return True
+
+    def on_error(self, status_code):
+        if status_code == 420:
+            # returning False in on_data disconnects the stream
+            print('ERROR 420')
+            return True
+
+
+if __name__ == '__main__':
+    l = StdOutListener()
+    auth = OAuthHandler(CONSUMER_KEY, CONSUMER_SECRET)
+    auth.set_access_token(ACCESS_TOKEN, ACCESS_TOKEN_SECRET)
+
+    stream = Stream(auth, l)
+    stream.filter(follow=['783214'])
